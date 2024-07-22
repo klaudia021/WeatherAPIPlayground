d8003f631e35e759a7f3f9127357016c40581af7
klaudia021 <61354324+klaudia021@users.noreply.github.com>
2024-07-22 14:20:20 +0200

    update dotenv require and remove logs

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv/config';
 import express from 'express';
+import { weatherService } from './weather/weatherService.js';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-import { fetchWeatherApiData } from './weather/weatherService.js';
+const weatherServiceInstance = new weatherService();
 
 app.use(express.static('public'));
 
 app.get('/api/data', async (req, res) => {
     try {
         const searchCity = req.query.city;
-        console.log('searchCity: ' + searchCity);
-        const weatherData = await fetchWeatherApiData(searchCity);
+        const weatherData = await weatherServiceInstance.fetchWeatherApiData(searchCity);
         res.json({ 
             message: 'Data fetched successfully',
             weatherData: weatherData 
         });
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching external data' });
+        res.status(400).json({ message: 'Error fetching external data' });
     }
 });
 
--- src/weather/weatherService.js
+++ src/weather/weatherService.js
@@ -1,30 +1,32 @@
 import axios from 'axios';
 import { GEO_DB_URL, GEO_DB_OPTIONS, WEATHER_API_URL } from '../utils/constants.js';
 
-export const fetchWeatherApiData = async (city) => {
-    try {
-        const { latitude, longitude } = await fetchCityCoordinates(city);
-        const response = await axios.get(`${WEATHER_API_URL}&lat=${latitude}&lon=${longitude}&units=metric`);
-        response.data.cityName = city;
-        console.log(response.data);
+export class weatherService {
+    async fetchWeatherApiData(city) {
+        try {
+            const { latitude, longitude } = await this.fetchCityCoordinates(city);
+            const response = await axios.get(`${WEATHER_API_URL}&lat=${latitude}&lon=${longitude}&units=metric`);
+            response.data.cityName = city.charAt(0).toUpperCase() + city.slice(1);
 
-        return response.data;
-    } catch (error) {
-        console.error(`Error in fetchWeatherApiData: ${error}`);
-        throw error;
-    }
-};
-
-const fetchCityCoordinates = async (city) => {
-    try {
-        const response = await axios.get(`${GEO_DB_URL}cities?namePrefix=${city}`, GEO_DB_OPTIONS);
-        if (!response.data || !response.data.data || response.data.data.length === 0) {
-            throw new Error('No data found');
+            return response.data;
+        } catch (error) {
+            console.error(`Error in fetchWeatherApiData: ${error}`);
+            throw error;
         }
-
-        return response.data.data[0];
-    } catch (error) {
-        console.error(`Error in fetchCityCoordinates: ${error}`);
-        throw error;
     }
+
+    async fetchCityCoordinates(city) {
+        try {
+            const response = await axios.get(`${GEO_DB_URL}cities?namePrefix=${city}`, GEO_DB_OPTIONS);
+            if (!response.data || !response.data.data || response.data.data.length === 0) {
+                throw new Error('No data found');
+            }
+    
+            return response.data.data[0];
+        } catch (error) {
+            console.error(`Error in fetchCityCoordinates: ${error}`);
+            throw error;
+        } 
+    }
+
 };
